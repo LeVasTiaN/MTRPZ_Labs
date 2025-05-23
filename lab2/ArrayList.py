64117868feadd862567b7b0d81b436f8c8984f7b
Lavryk Mykola <81701123+LeVasTiaN@users.noreply.github.com>
2025-05-23 16:00:42 +0200

    Added more methods to ArrayList.py

--- lab2/ArrayList.py
+++ lab2/ArrayList.py
@@ -14,3 +14,26 @@ class ArrayList:
         if index < 0 or index >= len(self._items):
             raise IndexError("Index out of bounds")
         return self._items.pop(index)
+
+    def insert(self, ch, index):
+        if not isinstance(ch, str) or len(ch) != 1:
+            raise ValueError("Only single characters allowed")
+        if index < 0 or index > len(self._items):
+            raise IndexError("Index out of bounds")
+        self._items.insert(index, ch)
+
+    def deleteAll(self, ch):
+        self._items = [x for x in self._items if x != ch]
+
+    def get(self, index):
+        if index < 0 or index >= len(self._items):
+            raise IndexError("Index out of bounds")
+        return self._items[index]
+
+    def clone(self):
+        copy = ArrayList()
+        copy._data = self._items.copy()
+        return copy
+
+    def reverse(self):
+        self._items.reverse()
