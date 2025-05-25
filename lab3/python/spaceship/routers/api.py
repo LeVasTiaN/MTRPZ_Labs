5f6ccd854a22bccd27d529e754dacb10f0b0e446
Lavryk Mykola <81701123+LeVasTiaN@users.noreply.github.com>
2025-05-25 16:41:14 +0200

    Added numpy to the project

--- lab3/python/spaceship/routers/api.py
+++ lab3/python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/mult-matrices")
+def mult_matrices():
+    a = numpy.random.rand(10, 10)
+    b = numpy.random.rand(10, 10)
+    result = numpy.dot(a, b)
+
+    return {
+        "a": a.tolist(),
+        "b": b.tolist(),
+        "product": result.tolist(),
+    }
